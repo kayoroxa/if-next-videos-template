4fd3a170ca5605188b608f7e3f70ff26e5e66024
Caio <kayoroxa@gmail.com>
2022-12-03 02:45:27 -0300

    if frases

---
+++ frases.js
@@ -0,0 +1,76 @@
+const dict = `
+https://y.yarn.co/25cbd0a7-263d-4c76-85bf-d91f9e4e39df.mp4
+I'm not finished.
+
+https://y.yarn.co/fd7855d5-8060-427e-b60f-a9a2fba8df65.mp4
+- I don't have the energy for you today.
+
+https://y.yarn.co/223b4f5d-be1e-4a56-bcf6-50ece5ba38e2.mp4
+You were attacked by a little white bird?
+
+https://y.yarn.co/335bd864-0d4f-4aa5-990f-ae43c7a26436.mp4
+Oh, I love the coziness.
+
+https://y.yarn.co/d707ef0f-61d6-46b0-be91-c13b540022e5.mp4
+You crazy and you need help.
+
+https://y.yarn.co/3b2ccdde-770b-4b54-9360-7507b905aacd.mp4
+There was a contest.
+
+https://y.yarn.co/0fbe0335-8553-4b2f-abb5-7a019d241427.mp4
+- You're teasing me. - Yeah, a little.
+
+https://y.yarn.co/b89a9174-3bf0-4f66-9153-cc29fc8afa71.mp4
+Things got pretty crazy yesterday, huh?
+
+https://y.yarn.co/ef548506-554d-4313-92d6-8afba5257e54.mp4
+It sounds like things got crazy.
+
+https://y.yarn.co/2ac94dde-905c-4711-8189-ee16614a6ada.mp4
+I know it sounds crazy.
+
+https://y.yarn.co/926b8e33-0a7c-4395-aa05-3b57789b310b.mp4
+Somebody... Somebody help me, please.
+
+https://y.yarn.co/b708e4f7-e735-443c-bef2-7b6cd0a55583.mp4
+No, I'm not. Your vision's just getting worse.
+
+https://y.yarn.co/a318f561-7175-4378-b203-11880ffa4562.mp4
+I did everything I could and I've only got 35 bucks.
+
+https://y.yarn.co/9d6862a3-a179-40d7-abaf-4b34b75c8c45.mp4
+Wait a minute! I'm terribly sorry, sir.
+
+https://y.yarn.co/1afeddca-e1f9-4520-8c5d-ac0fbd279e97.mp4
+I'm terribly, terribly sorry.
+
+https://y.yarn.co/4cff1047-f0ca-431e-9ed2-bd353a2a8037.mp4
+You're all sweaty.
+
+https://y.yarn.co/cc93b4f7-1325-4fe0-ba1a-66fd6c457765.mp4
+Isn't robbing a bank against the law?
+
+https://y.yarn.co/f9c4f2c6-e267-4557-9b6c-c12e416d928a.mp4
+You really should bake new ones.
+
+https://y.yarn.co/9523fa32-89ab-44c3-9d9d-cef9e72a4308.mp4
+So? How you doing?
+
+https://y.yarn.co/dd4a38a1-fc75-4ef5-9a7c-5adda4b31419.mp4
+What are we going to do?
+
+https://y.yarn.co/9962b9d1-4d04-4b16-8000-c7c789568ae5.mp4
+I don't know what I was so worried about last night.
+
+https://y.yarn.co/a23d0c32-9c48-4f51-b3e0-d94c55a2d0b6.mp4
+No. There are others.
+
+https://y.yarn.co/0d6864ec-3e4d-40cc-a00e-4273b999d0ff.mp4
+I don't want to lose you.
+
+https://y.yarn.co/6dd190f9-dd42-45c5-8da1-daf8327429e3.mp4
+That was really nice.
+
+`
+
+console.log(dict.split('\n\n').filter(Boolean).length)
